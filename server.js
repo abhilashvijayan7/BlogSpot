10a37e81a9ec2055bf16212d279d1f5a5c26bdab
abhilash <abhimadaparambath@gmail.com>
2023-11-20 20:04:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const articleRouter= require ('./routes/articles')
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/articles',articleRouter)
+
+app.get('/', function (req, res) {
+
+     const articles=[{
+        title:'Test article 1',
+        createdAt:new Date(),
+        description:'Test description 1'
+     },
+     {
+        title:'Test article 2',
+        createdAt:new Date(),
+        description:'Test description 2'
+     }]
+
+  res.render('index',{articles});
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
